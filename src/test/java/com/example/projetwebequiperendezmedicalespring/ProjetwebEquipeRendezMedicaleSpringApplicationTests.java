065d229c4dcb3f4bf872ea3ba04257ddc424fd7c
WildBabaloo <47115136+WildBabaloo@users.noreply.github.com>
2023-05-04 21:41:25 -0400

    Converted project to sping and added the original html files

---
+++ src/test/java/com/example/projetwebequiperendezmedicalespring/ProjetwebEquipeRendezMedicaleSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetwebequiperendezmedicalespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetwebEquipeRendezMedicaleSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
